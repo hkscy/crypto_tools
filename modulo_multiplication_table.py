f0326004c305755db4f690c12b168891e9fd8143
hicksc <kris_hicks@hotmail.com>
2014-12-04 12:04:16 +0000

    Update modulo_multiplication_table.py

--- modulo_multiplication_table.py
+++ modulo_multiplication_table.py
@@ -1,10 +1,13 @@
 # This program calculates a table of multiplications in modulo n
 # Multiplication in modular arithmetic is done a*b(mod n)
+# Invertible elements are identified where the product of a number and at least one other is 1.
 # Christopher Hicks : 4/12/14
 class colour: # Used for formatting text output via print.
-   	BOLD = '\033[1m'
+	BOLD = '\033[1m'
    	UNDERLINE = '\033[4m'
    	END = '\033[0m'
+
+invertible_elements = [] # Init. a list for holding group of invertible elements.
 
 n = raw_input(colour.BOLD + "Enter the value of n: ")
 n = int(n)
@@ -20,5 +23,10 @@ for i in range(n):
 	# Justify to the right of a two character width column
 	print colour.BOLD + repr(i).rjust(3) + colour.END, 
 	for j in range(n):
-		print repr((i*j)%n).rjust(3), # Ensure addition is modulo n
+		m = (i*j)%n
+		print str(m).rjust(3), # Ensure addition is modulo n
+		if m == 1: # Is invertible if product with at least one other is 1
+			invertible_elements.append(j)
 	print
+print 'The list of invertible elements in modulo ' + str(n) + ' is:'
+print sorted(invertible_elements)
